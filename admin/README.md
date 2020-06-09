907b44e68fb791ae6e5976aaf6882c67162318ed
ppxb <317842449@qq.com>
2020-06-09 14:20:55 +0800

    chore: add vuetify support

--- admin/README.md
+++ admin/README.md
@@ -1,19 +1,35 @@
-# admin
+# 后台管理界面
 
-## Project setup
+# tips
+
+1. 当使用 ts 开发并使用 Vuetify 做为组件库时，可能会报<code> Could not find a declaration file for module 'vuetify/lib' </code>的错误，需要修改项目根目录中的<code> tsconfig.json </code>。
+
+```
+// tsconfig.json
+
+{
+  "compilerOptions": {
+    "types": ["vuetify"]
+  }
+}
+```
+
+2. 使用 MD 风格样式图标时，可选用多种图标库。具体参考官网文档。
+
+# 安装依赖
+
 ```
 yarn install
 ```
 
-### Compiles and hot-reloads for development
+# 启动服务
+
 ```
 yarn serve
 ```
 
-### Compiles and minifies for production
+# 构建线上环境
+
 ```
 yarn build
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- admin/src/router/index.ts
+++ admin/src/router/index.ts
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
-  const routes: Array<RouteConfig> = [
+const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Dashboard',
+    component: Dashboard,
+    children: [
+      {
+        name: 'home',
+        path: '/',
+        component: async () => import('../views/Home.vue')
+      },
+      {
+        name: 'about',
+        path: '/about',
+        component: async () => import('../views/About.vue')
+      }
+    ]
   }
 ]
 
